530d328a31d37b7aa4aba314a12d0469f38f5f5f
Furkan Elmas <furkanelmas321@gmail.com>
2022-06-27 21:36:12 +0300

    + Added comment lines

--- Assets/Scripts/PlanetRotation.cs
+++ Assets/Scripts/PlanetRotation.cs
@@ -17,7 +17,7 @@ public class PlanetRotation : MonoBehaviour
     {
         StartCoroutine(AnimationCurveCor());
 
-        _animationCurve.AddKey(360, 360);
+        _animationCurve.AddKey(360, 360); // For calculate full tour of a planet
     }
 
     IEnumerator AnimationCurveCor()
@@ -25,15 +25,15 @@ public class PlanetRotation : MonoBehaviour
 
         while (true)
         {
-            transform.rotation = Quaternion.Euler(0, _animationCurve.Evaluate(_curveValue), 0);
+            transform.rotation = Quaternion.Euler(0, _animationCurve.Evaluate(_curveValue), 0); // Take corresponding value of keys (x-axis)
             transform.RotateAround(_centralPlanet.position, Vector3.up, Time.deltaTime * _spaceRotationSpeed);
 
-            _curveValue += Time.deltaTime * _selfRotationSpeed;
+            _curveValue += Time.deltaTime * _selfRotationSpeed; // Increase value of animation curve keys (y-axis)
 
-            _rotationLoopValue += Time.deltaTime * _spaceRotationSpeed;
-            if (_rotationLoopValue > 360)
+            _rotationLoopValue += Time.deltaTime * _spaceRotationSpeed; // Calculate total angle
+            if (_rotationLoopValue > 360) // Check for every 360 degree angle
             {
-                _rotationLoopValue = 0;
+                _rotationLoopValue = 0; // reset
                 Debug.Log($"{gameObject.name} has made a lap around {_centralPlanet.name}");
             }
 
